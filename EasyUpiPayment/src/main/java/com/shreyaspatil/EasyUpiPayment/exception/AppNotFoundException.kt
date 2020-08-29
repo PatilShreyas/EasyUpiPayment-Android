e62f0524d5b628c2476c39dd667e8e9bb944aabc
Shreyas <shreyaspatilg@gmail.com>
2020-08-29 22:26:34 +0530

    Create AppNotFoundException

---
+++ EasyUpiPayment/src/main/java/com/shreyaspatil/EasyUpiPayment/exception/AppNotFoundException.kt
@@ -0,0 +1,5 @@
+package com.shreyaspatil.easyupipayment.exception
+
+class AppNotFoundException(appPackage: String?) : Exception("""
+    No UPI app${appPackage?.let { " with package name '$it'" } ?: ""} exists on this device to perform this transaction.
+""".trimIndent())
