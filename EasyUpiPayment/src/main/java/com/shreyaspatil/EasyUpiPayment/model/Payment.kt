689be8e4ff8b2d36766bb645923c1984fab057fc
Shreyas <shreyaspatilg@gmail.com>
2020-08-29 22:27:47 +0530

    Convert models into data class

---
+++ EasyUpiPayment/src/main/java/com/shreyaspatil/EasyUpiPayment/model/Payment.kt
@@ -0,0 +1,15 @@
+package com.shreyaspatil.easyupipayment.model
+
+import java.io.Serializable
+
+data class Payment(
+	var currency: String,
+	var vpa: String,
+	var name: String,
+	var payeeMerchantCode: String?,
+	var txnId: String,
+	var txnRefId: String,
+	var description: String,
+	var amount: String,
+	var defaultPackage: String?
+) : Serializable
